a258294da826a438b7c2b28f275c6372349a90e7
Jens <jenniferk973@outlook.com>
2026-08-10 21:19:42 +0100

    Improve game instructions and readability
    Updated comments and added symbol explanations for each choice.

--- RPSLS.py
+++ RPSLS.py
@@ -10,6 +10,13 @@ def play():
  print('Rock | Paper | Scissors | Lizard | Spock ')
  print ('========================================')
  
-# This empty print is used to create space between the game header and the first question to improve readability for the player
+# This empty print is used to create space between the game header and the game explanation
 print()
+
+# This is so the players know which number correlates to which symbol
+ print ('1) ✊ - rock')
+ print ('2) ✋ - paper')
+ print ('3) ✌️ - scissors')
+ print ('4) 🦎 - lizard')
+ print ('5) 🖖 - spock')
 

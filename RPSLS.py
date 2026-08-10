c11613298b2f5a55b6e7687ece12e68e75ca1bef
Jens <jenniferk973@outlook.com>
2026-08-10 21:36:43 +0100

    Add outcomes for player choosing scissors

--- RPSLS.py
+++ RPSLS.py
@@ -81,3 +81,15 @@ computer = random.randint(1,5)
    print ('You lost')
  elif player == 2 and computer == 5:
    print ('You won')
+
+#Player chooses scissors
+ if player == 3 and computer == 1:
+   print ('You lost')
+ elif player == 3 and computer == 2:
+   print ('You won')
+ elif player == 3 and computer == 3:
+   print ('You tied')
+ elif player == 3 and computer == 4:
+   print ('You won')
+ elif player == 3 and computer == 5:
+   print ('You lost')

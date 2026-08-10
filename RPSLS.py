848f40f14747ec5f640cef6617b40ca8ce626fbf
Jens <jenniferk973@outlook.com>
2026-08-10 21:35:08 +0100

    Clean up comments related to random number generation
    Removed duplicate comment about random number generation and its range.

--- RPSLS.py
+++ RPSLS.py
@@ -40,6 +40,10 @@ print()
  else:
   print ('Wrong input')
 
+#Random is being used to generate random numbers but this code means only 1,2,3,4 or 5 will be picked
+
+computer = random.randint(1,5)
+
 # This is to give the randomly generated number a symbol so they player knows what the computer 'picked'
  if computer == 1:
   print ('CPU chose:✊ ')
@@ -54,6 +58,4 @@ print()
  else:
   print ('Error')
 
-#Random is being used to generate random numbers but this code means only 1,2,3,4 or 5 will be picked
 
-computer = random.randint(1,5)

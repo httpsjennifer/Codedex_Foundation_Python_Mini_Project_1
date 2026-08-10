678cf30af4111bd35a936503444d72528c96a52c
Jens <jenniferk973@outlook.com>
2026-08-10 21:16:50 +0100

    Add game header to play function
    Added a header to the play function for better game presentation.

--- RPSLS.py
+++ RPSLS.py
@@ -3,3 +3,9 @@ import random
 
 #This define function is used so that the game will loop once a round is over
 def play():
+
+ #This is used to create a header
+  
+ print('========================================')
+ print('Rock | Paper | Scissors | Lizard | Spock ')
+ print ('========================================')
